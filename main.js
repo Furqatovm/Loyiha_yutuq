b83e4ff203ac04107fde09db20d6b220cf2e8777
Furqatovm <muhammadyusuffurqatov91@gmail.com>
2025-03-26 13:16:21 +0500

    quiz app

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+let openn =document.getElementById("open");
+
+let closee =document.getElementById("close");
+let nav =document.getElementById("nav");
+openn.addEventListener("click", function(){
+    openn.style.display ="none";
+    closee.style.display ="block";
+    nav.style.display ="block"
+    
+})
